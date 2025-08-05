5c7fd710507458cd3ede0171b7c9582a2607830e
nau satou <naurinrin@gmail.com>
2025-08-05 16:04:42 +0900

    実行ボタン押下時に２つのexe同時処理まで

--- FDNSFilePasswordChecker/Form1.Designer.cs
+++ FDNSFilePasswordChecker/Form1.Designer.cs
@@ -8,6 +8,51 @@
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
+        /// 実行ボタン
+        /// </summary>
+        private System.Windows.Forms.Button btnSubmit;
+
+        /// <summary>
+        /// 選択フォルダのラベル
+        /// </summary>
+        private System.Windows.Forms.Label lblSourceFolder;
+
+        /// <summary>
+        /// 選択フォルダのテキストボックス
+        /// </summary>
+        private System.Windows.Forms.TextBox txtSourceFolder;
+
+        /// <summary>
+        /// 選択フォルダの参照ボタン
+        /// </summary>
+        private System.Windows.Forms.Button btnBrowseSource;
+
+        /// <summary>
+        /// ログ出力先のラベル
+        /// </summary>
+        private System.Windows.Forms.Label lblOutputFolder;
+
+        /// <summary>
+        /// ログ出力先のテキストボックス
+        /// </summary>
+        private System.Windows.Forms.TextBox txtOutputFolder;
+
+        /// <summary>
+        /// ログ出力先の参照ボタン
+        /// </summary>
+        private System.Windows.Forms.Button btnBrowseOutput;
+
+        /// <summary>
+        /// パスワード付きフォルダを除外するチェックボックス
+        /// </summary>
+        private System.Windows.Forms.CheckBox chkExcludePasswordFolders;
+
+        /// <summary>
+        /// タイトルラベル
+        /// </summary>
+        private System.Windows.Forms.Label lblTitle;
+
+        /// <summary>
         /// 使用中のリソースをすべてクリーンアップします。
         /// </summary>
         /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
@@ -28,10 +73,158 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblSourceFolder = new System.Windows.Forms.Label();
+            this.txtSourceFolder = new System.Windows.Forms.TextBox();
+            this.btnBrowseSource = new System.Windows.Forms.Button();
+            this.lblOutputFolder = new System.Windows.Forms.Label();
+            this.txtOutputFolder = new System.Windows.Forms.TextBox();
+            this.btnBrowseOutput = new System.Windows.Forms.Button();
+            this.chkExcludePasswordFolders = new System.Windows.Forms.CheckBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTitle.AutoSize = false;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblTitle.Location = new System.Drawing.Point(160, 50);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(480, 30);
+            this.lblTitle.TabIndex = 8;
+            this.lblTitle.Text = "PasswordChecker";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblSourceFolder
+            // 
+            this.lblSourceFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblSourceFolder.AutoSize = true;
+            this.lblSourceFolder.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSourceFolder.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblSourceFolder.Location = new System.Drawing.Point(160, 120);
+            this.lblSourceFolder.Name = "lblSourceFolder";
+            this.lblSourceFolder.Size = new System.Drawing.Size(65, 12);
+            this.lblSourceFolder.TabIndex = 1;
+            this.lblSourceFolder.Text = "選択フォルダ：";
+            // 
+            // txtSourceFolder
+            // 
+            this.txtSourceFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSourceFolder.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSourceFolder.Location = new System.Drawing.Point(240, 140);
+            this.txtSourceFolder.Name = "txtSourceFolder";
+            this.txtSourceFolder.Size = new System.Drawing.Size(400, 25);
+            this.txtSourceFolder.TabIndex = 2;
+            // 
+            // btnBrowseSource
+            // 
+            this.btnBrowseSource.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnBrowseSource.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnBrowseSource.FlatAppearance.BorderSize = 0;
+            this.btnBrowseSource.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBrowseSource.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseSource.ForeColor = System.Drawing.Color.White;
+            this.btnBrowseSource.Location = new System.Drawing.Point(160, 138);
+            this.btnBrowseSource.Name = "btnBrowseSource";
+            this.btnBrowseSource.Size = new System.Drawing.Size(70, 29);
+            this.btnBrowseSource.TabIndex = 3;
+            this.btnBrowseSource.Text = "参照";
+            this.btnBrowseSource.UseVisualStyleBackColor = false;
+            this.btnBrowseSource.Click += new System.EventHandler(this.btnBrowseSource_Click);
+            // 
+            // lblOutputFolder
+            // 
+            this.lblOutputFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblOutputFolder.AutoSize = true;
+            this.lblOutputFolder.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutputFolder.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.lblOutputFolder.Location = new System.Drawing.Point(160, 180);
+            this.lblOutputFolder.Name = "lblOutputFolder";
+            this.lblOutputFolder.Size = new System.Drawing.Size(65, 12);
+            this.lblOutputFolder.TabIndex = 4;
+            this.lblOutputFolder.Text = "ログ出力先：";
+            // 
+            // txtOutputFolder
+            // 
+            this.txtOutputFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOutputFolder.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutputFolder.Location = new System.Drawing.Point(240, 200);
+            this.txtOutputFolder.Name = "txtOutputFolder";
+            this.txtOutputFolder.Size = new System.Drawing.Size(400, 25);
+            this.txtOutputFolder.TabIndex = 5;
+            // 
+            // btnBrowseOutput
+            // 
+            this.btnBrowseOutput.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnBrowseOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnBrowseOutput.FlatAppearance.BorderSize = 0;
+            this.btnBrowseOutput.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBrowseOutput.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseOutput.ForeColor = System.Drawing.Color.White;
+            this.btnBrowseOutput.Location = new System.Drawing.Point(160, 198);
+            this.btnBrowseOutput.Name = "btnBrowseOutput";
+            this.btnBrowseOutput.Size = new System.Drawing.Size(70, 29);
+            this.btnBrowseOutput.TabIndex = 6;
+            this.btnBrowseOutput.Text = "参照";
+            this.btnBrowseOutput.UseVisualStyleBackColor = false;
+            this.btnBrowseOutput.Click += new System.EventHandler(this.btnBrowseOutput_Click);
+            // 
+            // chkExcludePasswordFolders
+            // 
+            this.chkExcludePasswordFolders.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.chkExcludePasswordFolders.AutoSize = true;
+            this.chkExcludePasswordFolders.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chkExcludePasswordFolders.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.chkExcludePasswordFolders.Location = new System.Drawing.Point(160, 240);
+            this.chkExcludePasswordFolders.Name = "chkExcludePasswordFolders";
+            this.chkExcludePasswordFolders.Size = new System.Drawing.Size(180, 16);
+            this.chkExcludePasswordFolders.TabIndex = 7;
+            this.chkExcludePasswordFolders.Text = "パスワード付きフォルダを除外する";
+            this.chkExcludePasswordFolders.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnSubmit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.btnSubmit.FlatAppearance.BorderSize = 0;
+            this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubmit.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.ForeColor = System.Drawing.Color.White;
+            this.btnSubmit.Location = new System.Drawing.Point(350, 320);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(100, 40);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "実行";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(249)))), ((int)(((byte)(250)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.chkExcludePasswordFolders);
+            this.Controls.Add(this.btnBrowseOutput);
+            this.Controls.Add(this.txtOutputFolder);
+            this.Controls.Add(this.lblOutputFolder);
+            this.Controls.Add(this.btnBrowseSource);
+            this.Controls.Add(this.txtSourceFolder);
+            this.Controls.Add(this.lblSourceFolder);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "Form1";
+            this.Text = "PasswordChecker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
 
         #endregion
--- FDNSFilePasswordChecker/Form1.cs
+++ FDNSFilePasswordChecker/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
 
 namespace FDNSFilePasswordChecker
 {
@@ -15,6 +17,143 @@ namespace FDNSFilePasswordChecker
         public Form1()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 実行ボタンのクリックイベントハンドラー
+        /// </summary>
+        /// <param name="sender">イベントの送信元オブジェクト</param>
+        /// <param name="e">イベント引数</param>
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 入力値の検証
+                if (string.IsNullOrWhiteSpace(txtSourceFolder.Text))
+                {
+                    MessageBox.Show("選択フォルダを指定してください。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(txtOutputFolder.Text))
+                {
+                    MessageBox.Show("ログ出力先を指定してください。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // フォルダの存在確認
+                if (!Directory.Exists(txtSourceFolder.Text))
+                {
+                    MessageBox.Show("指定された選択フォルダが存在しません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (!Directory.Exists(txtOutputFolder.Text))
+                {
+                    MessageBox.Show("指定されたログ出力先が存在しません。", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // exeファイルのパスを設定
+                string exePath1 = Path.Combine(Application.StartupPath, "FPCCmd.exe");
+                string exePath2 = Path.Combine(Application.StartupPath, "FPCNOCmd.exe");
+                
+                // exeファイルの存在確認
+                if (!File.Exists(exePath1))
+                {
+                    MessageBox.Show($"実行ファイルが見つかりません: {exePath1}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                if (!File.Exists(exePath2))
+                {
+                    MessageBox.Show($"実行ファイルが見つかりません: {exePath2}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // プロセス開始情報を設定
+                ProcessStartInfo startInfo1 = new ProcessStartInfo();
+                startInfo1.FileName = exePath1;
+                startInfo1.Arguments = $"\"{txtSourceFolder.Text}\" \"{txtOutputFolder.Text}\"";
+                
+                ProcessStartInfo startInfo2 = new ProcessStartInfo();
+                startInfo2.FileName = exePath2;
+                startInfo2.Arguments = $"\"{txtSourceFolder.Text}\" \"{txtOutputFolder.Text}\"";
+                
+                // チェックボックスの状態に応じて引数を追加
+                if (chkExcludePasswordFolders.Checked)
+                {
+                    startInfo1.Arguments += " --exclude-password";
+                    startInfo2.Arguments += " --exclude-password";
+                }
+
+                // 両方のプロセスを同時に開始
+                using (Process process1 = new Process())
+                using (Process process2 = new Process())
+                {
+                    process1.StartInfo = startInfo1;
+                    process2.StartInfo = startInfo2;
+                    
+                    // 同時並行で実行
+                    process1.Start();
+                    process2.Start();
+                    
+                    // プロセスの終了を待機（オプション）
+                    // process1.WaitForExit();
+                    // process2.WaitForExit();
+                }
+
+                // 成功メッセージを表示
+                string message = "FPCCmd.exeとFPCNOCmd.exeを同時に開始しました！";
+                if (chkExcludePasswordFolders.Checked)
+                {
+                    message += "\n(パスワード付きファイルは除外されています)";
+                }
+                
+                MessageBox.Show(message, "実行", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"実行中にエラーが発生しました: {ex.Message}", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// 選択フォルダの参照ボタンのクリックイベントハンドラー
+        /// </summary>
+        /// <param name="sender">イベントの送信元オブジェクト</param>
+        /// <param name="e">イベント引数</param>
+        private void btnBrowseSource_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                folderDialog.Description = "選択フォルダを指定してください";
+                folderDialog.ShowNewFolderButton = false;
+                
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtSourceFolder.Text = folderDialog.SelectedPath;
+                }
+            }
+        }
+
+        /// <summary>
+        /// ログ出力先の参照ボタンのクリックイベントハンドラー
+        /// </summary>
+        /// <param name="sender">イベントの送信元オブジェクト</param>
+        /// <param name="e">イベント引数</param>
+        private void btnBrowseOutput_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                folderDialog.Description = "ログ出力先フォルダを指定してください";
+                folderDialog.ShowNewFolderButton = true;
+                
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtOutputFolder.Text = folderDialog.SelectedPath;
+                }
+            }
         }
     }
 }
